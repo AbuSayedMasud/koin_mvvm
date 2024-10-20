e14dbe318dc38d115bd1754a623ca03c5cdf4e0a
AbuSayedMasud <68724752+AbuSayedMasud@users.noreply.github.com>
2024-10-20 16:53:12 +0600

    test

--- app/src/main/java/com/fintechhub/koin_mvvm/MainActivity.kt
+++ app/src/main/java/com/fintechhub/koin_mvvm/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.fintechhub.koin_mvvm
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -18,12 +21,17 @@ import com.fintechhub.koin_mvvm.screen.DetailsScreen
 import com.fintechhub.koin_mvvm.ui.theme.Koin_mvvmTheme
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Koin_mvvmTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                Scaffold(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 40.dp)
+                ) {
                     App()
                 }
             }
